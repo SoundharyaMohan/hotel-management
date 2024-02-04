c59a2b3860a394154eacd4b766109d052f8f5bb2
SoundharyaMohan <158762737+SoundharyaMohan@users.noreply.github.com>
2024-02-04 18:15:28 +0530

    Add files via upload

---
+++ vc.js
@@ -0,0 +1,13 @@
+<!doctype>
+<html>
+<head>
+<script>
+function call(){
+alert("THANKS FOR REGISTER")
+}
+</script>
+</head>
+<body>
+<input type="button" value="REGISTER" onclick()=call()/>
+</body>
+</html>
